6ada1ca5e165d44d89c93b97a568550bfaff1ce8
sibadima <148964526+sibadima@users.noreply.github.com>
2025-03-23 19:56:23 +0200

    Update SimpleLogger.java

--- SimpleLogger.java
+++ SimpleLogger.java
@@ -3,13 +3,10 @@ package loggers;
 import java.time.LocalDateTime;
 
 public class SimpleLogger implements Logger {
-    private int counter = 0;
+
     @Override
     public void log(String msg) {
-        counter++;
-        System.out.printf("%s#%d [%s] %s\n",
-                msg.toLowerCase().contains("error") ? "ERROR" : "INFO",
-                counter,
+        System.out.printf("[%s] %s\n",
                 LocalDateTime.now(),
                 msg);
     }
