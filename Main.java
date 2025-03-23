33a2f157afaa486eef7a93f26fba30649d9243a2
sibadima <148964526+sibadima@users.noreply.github.com>
2025-03-23 19:55:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package loggers;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Реализация интерфейса SimpleLogger");
         Logger logger = new SimpleLogger();
         logger.log("Сходил в магазин");
         Thread.sleep(1000);
@@ -11,6 +11,17 @@ public class Main {
         logger.log("Не хватило денег. ERROR");
         Thread.sleep(1000);
         logger.log("Вечер не удался");
+        Thread.sleep(1000);
+
+        System.out.println("Реализация интерфейса SmartLogger");
+        Logger smartLogger = new SmartLogger();
+        smartLogger.log("Сходил в магазин");
+        Thread.sleep(1000);
+        smartLogger.log("Купил бутылочку пива");
+        Thread.sleep(1000);
+        smartLogger.log("Не хватило денег. ERROR");
+        Thread.sleep(1000);
+        smartLogger.log("Вечер не удался");
         Thread.sleep(1000);
     }
 }
