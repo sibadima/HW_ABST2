1439af303efed2811e7c9453bbf38f1957e339bf
sibadima <148964526+sibadima@users.noreply.github.com>
2025-03-23 19:55:11 +0200

    Update SmartLogger.java

--- SmartLogger.java
+++ SmartLogger.java
@@ -1,9 +1,16 @@
 package loggers;
 
-public class SmartLogger implements Logger {
+import java.time.LocalDateTime;
 
+public class SmartLogger implements Logger {
+    private int counter = 0;
     @Override
     public void log(String msg) {
-
+        counter++;
+        System.out.printf("%s#%d [%s] %s\n",
+                msg.toLowerCase().contains("error") ? "ERROR" : "INFO",
+                counter,
+                LocalDateTime.now(),
+                msg);
     }
 }
